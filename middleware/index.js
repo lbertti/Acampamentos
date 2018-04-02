0f2111d77eedf9017de230618fbb2fa5eebb6221
Lucas <bertti@gmail.com>
2018-04-02 21:33:15 +0000

    Alteração no nome escrito na Landing Page; Alterações em algumas mensagens do Flash.

--- middleware/index.js
+++ middleware/index.js
@@ -9,18 +9,21 @@ middlewareObj.isTheCampOwner = function(req, res, next){
         Campground.findById(req.params.id, function(err, foundCampground){
             if(err){
                 console.log("ERRO AO TENTAR ENCONTRAR O CAMPGROUND NA VERIFICAÇÃO DO OWNER - isTheOwner(): " + err);
+                req.flash("error", "Ocorreu um eror ao tentar buscar o Acampamento no BD. Notifique o administrador com a mensagem de erro que segue: <<<< " + err + " >>>>"); //FLASH MESSAGE
                 res.redirect("back");
             } else {
                 if (foundCampground.author.id.equals(req.user._id)) {//se for o criador do campground
                     next();
                 } else {  //senão, redireciona e não deixa editar
                     console.log("O USUÁRIO TENTOU EXECUTAR UMA AÇÃO SOBRE ALGUM OBJETO QUE NÃO LHE PERTENCE! - isTheOwner()");
+                    req.flash("error", "O usuário logado não possui permissão para executar essa ação!"); //FLASH MESSAGE
                     res.redirect("back");
                 }
             }
         });
     } else {
         console.log("É NECESSÁRIO ESTAR LOGADO PRA EXECUTAR ESSA AÇÃO - isTheOwner()");
+        req.flash("error", "Você deve estar logado para executar essa ação!"); //FLASH MESSAGE
         res.redirect("back");
     }
 }
@@ -32,6 +35,7 @@ middlewareObj.isTheCommentOwner = function(req, res, next){
         Comment.findById(req.params.idComment, function(err, foundComment){
             if(err){
                 console.log("ERRO AO TENTAR ENCONTRAR O COMMENT NA VERIFICAÇÃO DO OWNER - comment->isTheOwner(): " + err);
+                req.flash("error", "Ocorreu um eror ao tentar buscar o Comentário no BD. Notifique o administrador com a mensagem de erro que segue: <<<< " + err + " >>>>"); //FLASH MESSAGE
                 res.redirect("back");
             } else {
                 if (foundComment.author.id.equals(req.user._id)) {//se for o criador do campground
